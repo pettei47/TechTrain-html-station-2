5d9aeeb77d69ca8dcfb3f5b40cab5eb9f0566181
pettei47 <kitagawa.teppei.33c@gmail.com>
2022-08-22 00:15:19 +0900

    13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,9 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const checked = document.getElementById('check').checked
+  if (checked) {
+    document.getElementById("text").style.backgroundColor = "red"
+  } else {
+    document.getElementById("text").style.backgroundColor = "white"
+  }
 }
